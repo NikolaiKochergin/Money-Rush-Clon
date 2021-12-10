09e57e26a02f12337bdaf3e182e9cf58d5de7e70
MrYapetzku <kocherginizer@gmail.com>
2021-12-10 17:35:35 +0300

    Mover final fixing

--- Assets/Scripts/Player/Model/Mover.cs
+++ Assets/Scripts/Player/Model/Mover.cs
@@ -5,29 +5,24 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private Player _player;
     [SerializeField] [Min(0)] private float _roadWidth;
-    [SerializeField] [Range(0, 1)] private float _sensetivity;
+    [SerializeField] [Range(0, 100)] private float _sensetivity;
     [SerializeField] [Min(0)] private float _turnSpeed;
 
     private PlayerInput _input;
-    private float _speed;
     private float _deltaX;
-    private bool _isPressed;
-
     private Vector3 _targetDirection;
     private Vector3 _currentDirection;
 
-    public float Speed => Vector3.Magnitude(_currentDirection) * _speed;
+    public float Speed => Vector3.Magnitude(_currentDirection) * _player.Speed;
 
     private void OnEnable()
     {
-        _speed = _player.Speed;
         _targetDirection = Vector3.forward;
         _currentDirection = Vector3.forward;
     }
 
     private void OnDisable()
     {
-        _targetDirection = Vector3.forward;
         _currentDirection = Vector3.forward;
         transform.rotation = Quaternion.LookRotation(_currentDirection);
     }
@@ -35,39 +30,27 @@ public class Mover : MonoBehaviour
     private void Update()
     {
         _deltaX = _input.Player.MoveX.ReadValue<float>();
-        _isPressed = _input.Player.Click.IsPressed();
-    }
+        if (_input.Player.Click.IsPressed())
+            _targetDirection.x = Mathf.Clamp(_deltaX * _sensetivity, -1, 1);
+        else
+            _targetDirection.x = 0;
 
-    private void FixedUpdate()
-    {
-        _targetDirection.z = 1;
-
-        if (transform.position.x + _speed/(Mathf.PI * _turnSpeed) >= _roadWidth / 2)
+        if (transform.position.x >= _roadWidth / 2)
             _targetDirection.x = Mathf.Clamp(_targetDirection.x, -1, 0);
 
-        if (transform.position.x - _speed / (Mathf.PI * _turnSpeed) <= -_roadWidth / 2)
+        if (transform.position.x <= -_roadWidth / 2)
             _targetDirection.x = Mathf.Clamp(_targetDirection.x, 0, 1);
 
-        _currentDirection = Vector3.Lerp(_currentDirection, _targetDirection, _turnSpeed * Time.fixedDeltaTime);
-        transform.position += _currentDirection * _speed * Time.fixedDeltaTime;
+        _currentDirection = Vector3.Lerp(_currentDirection, _targetDirection, _turnSpeed * Time.deltaTime);
+        transform.position += _player.Speed * Time.deltaTime * _currentDirection;
 
-        transform.rotation = Quaternion.LookRotation(_currentDirection);
+        float positionX = Mathf.Clamp(transform.position.x, -_roadWidth / 2, _roadWidth / 2);
+        
+        transform.SetPositionAndRotation(new Vector3(positionX, transform.position.y, transform.position.z), Quaternion.LookRotation(_currentDirection));
     }
 
     public void Initialization(PlayerInput input)
     {
-        if (input == null)
-            throw new ArgumentNullException(nameof(input));
-
-        _input = input;
-        _input.Player.MoveX.performed += ctx => OnMoveX();
-    }
-
-    private void OnMoveX()
-    {
-        if (_isPressed)
-            _targetDirection.x = Mathf.Clamp(_deltaX * _sensetivity, -1, 1);
-        else
-            _targetDirection.x = 0;
+        _input = input ?? throw new ArgumentNullException(nameof(input));
     }
 }
